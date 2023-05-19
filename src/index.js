c6633b3ceedab888f3dcd62b8a10c3a129a1411c
IhorMasechko <igor.masechko007@gmail.com>
2023-05-19 23:45:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="tweets-test-task">
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </PersistGate>
